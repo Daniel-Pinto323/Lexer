f5d15696b8dbdb3618c3c386573ba5501af4d30e
Daniel Pinto <daniel.pinto@ufl.edu>
2022-01-31 20:04:27 -0500

    still working on STRING_LITs

--- src/edu/ufl/cise/plc/lexer.java
+++ src/edu/ufl/cise/plc/lexer.java
@@ -14,6 +14,7 @@ String currTok  = "";
 
         int row = 0;
         int column = 0;
+        boolean error = false;
 
 
         //this function will break the tokens up and store them in a container
@@ -22,42 +23,68 @@ String currTok  = "";
 
                 // STRING LITs
             if (program.charAt(i) == '\"') {
-                do {
+                int startRow = row;
+                int startCol = column;
+                boolean danglingQuote = true;
+                while (!error) {
                     i++;
                     column++;
+                    if (program.charAt(i) == '\\') {
+                        // ESCAPE SEQUENCES
+                        currTok += "\\";
+                        switch (program.charAt(i + 1)) {
+                            case 'b':
+                                currTok += "b";
+                                break;
+                            case 't':
+                                currTok += "t";
+                                break;
+                            case 'n':
+                                currTok += "n";
+                                row++;
+                                column = 0;
+                                break;
+                            case 'f':
+                                currTok += "f";
+                                break;
+                            case 'r':
+                                currTok += "r";
+                                row++;
+                                column = 0;
+                                break;
+                            case '\"':
+                                currTok += "\"";
+                                break;
+                            case '\'':
+                                currTok += "\'";
+                                break;
+                            case '\\':
+                                currTok += "\\";
+                                break;
+                            default:
+                                tokens.add(new token(currTok, startRow, startCol, IToken.Kind.ERROR));
+                                error = true;
+                        }
+                        // adding to i and column to account for escape sequences being 2 characters
+                        i++;
+                        column++;
+                    }
+                    else if (program.charAt(i) == '\"') {
+                        danglingQuote = !danglingQuote;
+                    }
                     currTok += program.charAt(i);
-                } while (program.charAt(i) != '\"');
 
+                    // if while loop iterates to end of program without finding end quote -> add ERROR token + break
+                    // potentially requires throwing the error rather than adding ERROR token
+                    if (i == program.length() - 1 && danglingQuote) {
+                        tokens.add(new token(currTok, startRow, startCol, IToken.Kind.ERROR));
+                        error = true;
+                        break;
+                    }
+                }
                 tokens.add(new token(currTok, row, column, IToken.Kind.STRING_LIT));
+                currTok = "";
             }
-            /*if (program.charAt(i) == '\\') {
-
-                if (program.charAt(i + 1) == 'b') {
-                    tokens.add(new token("\\b", row, column));
-                    i++;//skips the following character
-                    column++;//accounts for the column that was skipped
-                }
-                else if (program.charAt(i + 1) == 't'){
-                    tokens.add(new token("\\t", row, column));
-                    i++;//skips the following character
-                    column++;//accounts for the column that was skipped
-                }
-                else if (program.charAt(i + 1) == 'n'){
-                    tokens.add(new token("\\n", row, column));
-                    i++;//skips the following character
-                    column++;//accounts for the column that was skipped
-                }
-                else if (program.charAt(i + 1) == 'f'){
-                    tokens.add(new token("\\f", row, column));
-                    i++;//skips the following character
-                    column++;//accounts for the column that was skipped
-                }
-                else if (program.charAt(i + 1) == 'r'){
-                    tokens.add(new token("\\r", row, column));
-                    i++;//skips the following character
-                    column++;//accounts for the column that was skipped
-                }
-            }*/
 
             //IDENTIFIERS AND RESERVED WORDS
             if (Character.isLetter(program.charAt(i))) {
@@ -65,10 +92,10 @@ String currTok  = "";
 
                 while(Character.isLetter(program.charAt(i)) || Character.isDigit(program.charAt(i))){
                     currTok += program.charAt(i);
-                        i+=1;
+                    i++;
                  }
 
-                tokens.add(new token(currTok, row, column));
+                tokens.add(new token(currTok, row, column, IToken.Kind.IDENT));
                }
 
             //INT-LIT
