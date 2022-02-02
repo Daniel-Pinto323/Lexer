f74229b6501a6e400c574bcdce87be0a4ab25717
Daniel Pinto <daniel.pinto@ufl.edu>
2022-02-01 21:15:08 -0500

    Improving STRING_LIT handling + beginning '@' and '#' handling

--- src/edu/ufl/cise/plc/lexer.java
+++ src/edu/ufl/cise/plc/lexer.java
@@ -14,21 +14,21 @@ String currTok  = "";
 
         int row = 0;
         int column = 0;
-        boolean error = false;
-
 
         //this function will break the tokens up and store them in a container
-
         for(int i = 0; i < program.length(); i++) {
+            boolean continueFlag = true;
 
-                // STRING LITs
+            // STRING LITs (begin with open quotes ' " ')
             if (program.charAt(i) == '\"') {
                 int startRow = row;
                 int startCol = column;
                 boolean danglingQuote = true;
-                while (!error) {
-                    i++;
-                    column++;
+                i++;
+                column++;
+
+                // while still within double quotes
+                while (continueFlag) {
                     if (program.charAt(i) == '\\') {
                         // ESCAPE SEQUENCES
                         currTok += "\\";
@@ -63,7 +63,8 @@ String currTok  = "";
                                 break;
                             default:
                                 tokens.add(new token(currTok, startRow, startCol, IToken.Kind.ERROR));
-                                error = true;
+                                // throw PLCException(row, column, "ERROR: invalid escape sequence");
+                                continueFlag = false;
                         }
                         // adding to i and column to account for escape sequences being 2 characters
                         i++;
@@ -71,19 +72,38 @@ String currTok  = "";
                     }
                     else if (program.charAt(i) == '\"') {
                         danglingQuote = !danglingQuote;
+                        i++;
+                        column++;
                     }
-                    currTok += program.charAt(i);
 
                     // if while loop iterates to end of program without finding end quote -> add ERROR token + break
                     // potentially requires throwing the error rather than adding ERROR token
-                    if (i == program.length() - 1 && danglingQuote) {
-                        tokens.add(new token(currTok, startRow, startCol, IToken.Kind.ERROR));
-                        error = true;
-                        break;
+                    if (i == program.length() - 1) {
+                        if (danglingQuote) {
+                            tokens.add(new token(currTok, startRow, startCol, IToken.Kind.ERROR));
+                        }
+                        else {
+                            tokens.add(new token(currTok, row, column, IToken.Kind.STRING_LIT));
+                        }
+                        continueFlag = false;
                     }
+
+                    currTok += program.charAt(i);
                 }
-                tokens.add(new token(currTok, row, column, IToken.Kind.STRING_LIT));
                 currTok = "";
+            }
+
+            if (program.charAt(i) == '@') {
+                // handling invalid character
+                tokens.add(new token(currTok, row, column, IToken.Kind.ERROR));
+                // throw new LexicalException("exception"); // not sure how to throw exceptions properly
+            }
+            if (program.charAt(i) == '#') {
+                // handling comments
+                while(program.charAt(i) != '\\') {
+                    i++;
+                    column++;
+                }
             }
 
             //IDENTIFIERS AND RESERVED WORDS
@@ -96,7 +116,7 @@ String currTok  = "";
                  }
 
                 tokens.add(new token(currTok, row, column, IToken.Kind.IDENT));
-               }
+            }
 
             //INT-LIT
 
@@ -112,7 +132,6 @@ String currTok  = "";
 
             //ASSIGN AND EQUALS
             if (program.charAt(i) == '=') {
-
                 if (program.charAt(i + 1) == '=') {
                     currTok = "==";
                     tokens.add(new token(currTok, row, column, IToken.Kind.EQUALS));
@@ -127,7 +146,7 @@ String currTok  = "";
 
           column++;
         }
-       tokens.add(new token(" ", row, column, IToken.Kind.EOF));
+       tokens.add(new token(null, row, column, IToken.Kind.EOF));
     }
 
     public IToken next(){
